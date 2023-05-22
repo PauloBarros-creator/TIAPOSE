ed4d52f3f2daec5dcda5f5a31af18ed90d557c2e
duartebrandao <79524904+duartebrandao@users.noreply.github.com>
2023-05-22 21:57:02 +0100

    atualização modelos

--- projeto-final-tp1-g12/otimizar(hill_climb).R
+++ projeto-final-tp1-g12/otimizar(hill_climb).R
@@ -12,6 +12,10 @@ source("eval.R")
 sales_pred1 <- predict_ksvm_stella()
 sales_pred2 <- predict_ksvm_bud()
 
+max_previsto_stella = max(predict_ksvm_stella())
+max_previsto_bud = max(predict_ksvm_bud())
+
+
 N=1000 # number of searches
 REPORT=N/20 # report results
 
@@ -22,6 +26,15 @@ upper=getUpperLimit(c(sales_pred1,sales_pred2)) #  upper bounds
 rchange1=function(par,lower,upper) # change for hclimbing
 { hchange(par,lower=lower,upper=upper,rnorm,mean=0,sd=0.25,round=FALSE) }
 
-HC=hclimbing(par=upper,fn=eval(),change=rchange1,lower=lower,upper=upper,type="max",
+s0=c(rep(sample(0:max_previsto_stella +max_previsto_stella*0.5, 1),7),rep(sample(0:max_previsto_bud + max_previsto_bud*0.5,1),7),rep(sample(0:72, 1),7),rep(sample(0:60, 1),7),rep(sample(0:90, 1),7),rep(sample(0:120, 1),7))
+
+
+
+cat("hill climbing search D=",D,"(iters=",N,")\n")
+# initial solution: 
+HC=hclimbing(par=s0,fn=eval,change=rchange1,lower=lower,upper=upper,type="max",
              control=list(maxit=N,REPORT=REPORT,digits=2))
+#L=-eval(HC$par)
+#if(L>best) { BESTSA=HC; best=L;}
+#}
 cat("best solution:",HC$sol,"evaluation function",HC$eval,"\n")
--- projeto-final-tp1-g12/otimizar(sann).R
+++ projeto-final-tp1-g12/otimizar(sann).R
@@ -6,14 +6,10 @@ source("eval.R")
 source("hill.R")
 
 N=1000 # searches
-REPORT=N/20 # report results
+REPORT=N/10 # report results
 
-rchange1=function(par,lower,upper) # change for hclimbing
-{ hchange(par,lower=lower,upper=upper,rnorm,mean=0,sd=0.25,round=TRUE) }
 
-# slight change of a real par under a normal u(0,0.5) function:
-rchange2=function(par) # change for hclimbing
-{ hchange(par,lower=lower,upper=upper,rnorm,mean=0,sd=0.5,round=TRUE) }
+
 
 sales_pred1 = predict_ksvm_stella()
 sales_pred2 = predict_ksvm_bud()
@@ -25,12 +21,16 @@ D=42 # dimension
 lower=rep(0,D) # lower bounds
 upper=getUpperLimit(c(sales_pred1,sales_pred2)) #  upper bounds
 
-eval1=function(s) return(-eval(s))
+# slight change of a real par under a normal u(0,0.5) function:
+rchange2=function(par) # change for hclimbing
+{ hchange(par,lower=lower,upper=upper,rnorm,mean=0,sd=0.5,round=TRUE) }
+
+eval1=function(s) return(eval(s))
 sann <- function(){
   
   s0=c(rep(sample(0:max_previsto_stella +max_previsto_stella*0.5, 1),7),rep(sample(0:max_previsto_bud + max_previsto_bud*0.5,1),7),rep(sample(0:72, 1),7),rep(sample(0:60, 1),7),rep(sample(0:90, 1),7),rep(sample(0:120, 1),7))
   cat("Simulated Annealing search D=",D,"(iters=",N,")\n")
-  CSANN=list(maxit=N,temp=2000,trace=TRUE)
+  CSANN=list(maxit=N,temp=2000,trace=TRUE, fnscale= -1)
   SA=optim(par=s0,fn=eval1,method="SANN",gr=rchange2,control=CSANN)
   #L=-eval(SA$par)
   #if(L>best) { BESTSA=SA; best=L;}
